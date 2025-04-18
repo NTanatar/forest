f769609ee6c37397a91c18c0832ef0c475590bf0
Natalia Tanatarova <natalia.tanatarova@sigma.software>
2025-04-18 17:34:15 +0200

    second proxy

--- src/main/java/com/nata/forest/ForestApplication.java
+++ src/main/java/com/nata/forest/ForestApplication.java
@@ -1,6 +1,10 @@
 package com.nata.forest;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,31 +22,46 @@ public class ForestApplication {
         return args -> {
             var storyService = new DefaultStoryService();
 
-            // proxy example one (JDK proxy mechanism):
-            var firstProxy = createProxy(storyService);
+            System.out.println("--- proxy one: JDK proxy mechanism ---");
+            var firstProxy = createProxyOne(storyService);
             firstProxy.create();
             firstProxy.proceed();
 
+            System.out.println("--- proxy two: spring proxy mechanism ---");
+            var secondProxy = createProxyTwo(storyService);
+            secondProxy.create();
+            secondProxy.proceed();
         };
     }
 
-    private StoryService createProxy(StoryService storyService) {
+    StoryService createProxyOne(StoryService target) {
         return (StoryService) Proxy
             .newProxyInstance(
-                storyService.getClass().getClassLoader(),
-                storyService.getClass().getInterfaces(),
-                (proxy, method, args1) -> {
-                    if (method.getAnnotation(Mystery.class) != null) {
-                        System.out.println("mysterious things starting");
-                    }
-                    try {
-                        return method.invoke(storyService, args1);
+                target.getClass().getClassLoader(),
+                target.getClass().getInterfaces(),
+                (proxy, method, args) -> wrapInvocation(target, method, args));
+    }
 
-                    } finally {
-                        if (method.getAnnotation(Mystery.class) != null) {
-                            System.out.println("mysterious things ending");
-                        }
-                    }
-                });
+    StoryService createProxyTwo(StoryService target) {
+        var pf = new ProxyFactory();
+        pf.setInterfaces(target.getClass().getInterfaces());
+        pf.setTarget(target);
+        pf.addAdvice((MethodInterceptor) inv -> wrapInvocation(target, inv.getMethod(), inv.getArguments()));
+
+        return (StoryService) pf.getProxy(getClass().getClassLoader());
+    }
+
+    Object wrapInvocation(Object target, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException {
+        if (method.getAnnotation(Mystery.class) != null) {
+            System.out.println("mysterious things starting");
+        }
+        try {
+            return method.invoke(target, args);
+
+        } finally {
+            if (method.getAnnotation(Mystery.class) != null) {
+                System.out.println("mysterious things ending");
+            }
+        }
     }
 }
