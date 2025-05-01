c3db071d475adcfc4c8580c39bcebb9e5b22d178
Natalia Tanatarova <natalia.tanatarova@sigma.software>
2025-05-01 15:19:49 +0200

    another simple endpoint

--- bird/src/main/java/com/nata/bird/controller/BirdController.java
+++ bird/src/main/java/com/nata/bird/controller/BirdController.java
@@ -2,6 +2,7 @@ package com.nata.bird.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +12,11 @@ public class BirdController {
     @ResponseBody
     public String getBirdSound() {
         return "chirps";
+    }
+
+    @PostMapping("/feed")
+    @ResponseBody
+    public FeedResponse feed() {
+        return new FeedResponse("thanks");
     }
 }
---
+++ bird/src/main/java/com/nata/bird/controller/FeedResponse.java
@@ -0,0 +1,10 @@
+package com.nata.bird.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FeedResponse {
+    private String song;
+}
