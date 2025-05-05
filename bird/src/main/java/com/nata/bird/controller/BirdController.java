f6eda620edf18de59576fa0b121412bba9f76d0e
Natalia Tanatarova <natalia.tanatarova@sigma.software>
2025-05-05 12:32:27 +0200

    trying sockets

--- bird/src/main/java/com/nata/bird/controller/BirdController.java
+++ bird/src/main/java/com/nata/bird/controller/BirdController.java
@@ -4,10 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@RequestMapping("/bird")
 public class BirdController {
 
     @GetMapping("/sound")
--- sockets/src/main/java/com/nata/sockets/BirdFeeder.java
+++ sockets/src/main/java/com/nata/sockets/BirdFeeder.java
@@ -4,25 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import lombok.Getter;
 
+@Getter
 public class BirdFeeder {
-
-    private OutputStream out;
-    private InputStream in;
-
-    public void startConnection(String ip, int port) throws IOException {
-        Socket clientSocket = new Socket(ip, port);
-
-        out = clientSocket.getOutputStream();
-        in = clientSocket.getInputStream();
-    }
-
-    public void sendMessage(String msg) throws IOException {
-        out.write(msg.getBytes());
-        dump(in);
-    }
 
     static void dump(InputStream is) throws IOException {
         try (var reader = new InputStreamReader(is);
@@ -35,8 +23,15 @@ public class BirdFeeder {
     }
 
     public static void main(String[] args) throws IOException {
-        BirdFeeder client = new BirdFeeder();
-        client.startConnection("127.0.0.1", 8080);
-        client.sendMessage("GET /sound HTTP/1.1\n\n");
+
+        try (Socket socket = new Socket("127.0.0.1", 8080)) {
+            var writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            writer.println("GET /bird/sound HTTP/1.1");
+            //writer.println("Host: localhost:8080");
+            writer.flush();
+
+            System.out.println("waiting for response...");
+            dump(socket.getInputStream());
+        }
     }
 }
