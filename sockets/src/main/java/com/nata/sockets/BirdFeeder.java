36f8ae63acfa59b3f3ffbe38fbac18355a5b086e
Natalia Tanatarova <natalia.tanatarova@sigma.software>
2025-05-18 21:12:55 +0200

    (sockets) calling get endpoint

--- sockets/src/main/java/com/nata/sockets/BirdFeeder.java
+++ sockets/src/main/java/com/nata/sockets/BirdFeeder.java
@@ -1,37 +1,42 @@
 package com.nata.sockets;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.net.Socket;
 import lombok.Getter;
 
 @Getter
 public class BirdFeeder {
 
-    static void dump(InputStream is) throws IOException {
-        try (var reader = new InputStreamReader(is);
-        var bufferedReader = new BufferedReader(reader)) {
-            var line = "";
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+    static void feedBird() throws IOException {
+        String GET_REQUEST = "GET /bird/sound HTTP/1.1\r\nHost:localhost\r\n\r\n";
+        Socket socket = new Socket("localhost", 8080);
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+
+        try {
+            os.write(GET_REQUEST.getBytes());
+            socket.shutdownOutput();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int b;
+        while (true) {
+            try {
+                if ((b = is.read()) == -1) {
+                    break;
+                }
+                System.out.print((char) b);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
 
+
     public static void main(String[] args) throws IOException {
-
-        try (Socket socket = new Socket("127.0.0.1", 8080)) {
-            var writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            writer.println("GET /bird/sound HTTP/1.1");
-            //writer.println("Host: localhost:8080");
-            writer.flush();
-
-            System.out.println("waiting for response...");
-            dump(socket.getInputStream());
-        }
+        feedBird();
     }
 }
