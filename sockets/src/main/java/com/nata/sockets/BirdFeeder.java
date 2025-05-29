2f31b02f36d2bea93299b46d97fa4b87a1bd53ae
Natalia Tanatarova <natalia.tanatarova@sigma.software>
2025-05-29 19:47:20 +0200

    sockets and https

--- sockets/src/main/java/com/nata/sockets/BirdFeeder.java
+++ sockets/src/main/java/com/nata/sockets/BirdFeeder.java
@@ -5,22 +5,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.Socket;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import lombok.Getter;
 
 @Getter
 public class BirdFeeder {
 
     static final String GET_REQUEST = "GET /bird/sound HTTP/1.1\r\nHost:localhost\r\n\r\n";
-
     static final String POST_REQUEST = "POST /bird/feed HTTP/1.1\r\nHost:localhost\r\nContent-Type: application/json\r\nContent-Length: 21\r\n\r\n{\"foodName\":\"bread\"}";
 
     static void send(String request) throws IOException {
-        Socket socket = new Socket("localhost", 8080);
+        //Socket socket = new Socket("localhost", 8080);
+
+        SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) sslsocketfactory
+            .createSocket("localhost", 8443);
+
         InputStream is = socket.getInputStream();
         OutputStream os = socket.getOutputStream();
 
         try {
+            socket.startHandshake();
+
             os.write(request.getBytes());
             socket.shutdownOutput();
         } catch (IOException e) {
