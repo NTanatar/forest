4c73e8c7c22f92aa1b08f7c78027d37293f15345
Natalia Tanatarova <natalia.tanatarova@sigma.software>
2025-06-02 23:29:38 +0200

    refactor and time measurement

---
+++ nasa/src/main/java/com/nata/nasa/Image.java
@@ -0,0 +1,10 @@
+package com.nata.nasa;
+
+import lombok.Data;
+
+@Data
+public class Image {
+    private String url;
+    private int id;
+    private long size;
+}
--- nasa/src/main/java/com/nata/nasa/NasaService.java
+++ nasa/src/main/java/com/nata/nasa/NasaService.java
@@ -5,6 +5,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.OptionalLong;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -16,56 +17,66 @@ public class NasaService {
     private final static String BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos";
     private final static String API_KEY = "mYISaCMwm5xo8Z8ExYGzg0xJuecrU4ab0TScQ2fT";
 
-    public static ArrayList<String> toImageSources(String content) {
-        ArrayList<String> result = new ArrayList<>();
+    public static ArrayList<Image> toImages(String content) {
+        ArrayList<Image> result = new ArrayList<>();
         JSONObject json = new JSONObject(content);
         JSONArray photos = json.optJSONArray("photos");
         for (int i = 0; i < photos.length(); i++) {
             JSONObject photo = photos.optJSONObject(i);
-            result.add(photo.get("img_src").toString());
+
+            Image image = new Image();
+            image.setId(Integer.parseInt(photo.get("id").toString()));
+            image.setUrl(photo.get("img_src").toString());
+
+            result.add(image);
         }
         return result;
     }
 
-    public static long getImageSize(HttpClient client, String imageSource) {
-        HttpRequest first = HttpRequest.newBuilder().uri(URI.create(imageSource)).build();
+    public static void fetchSize(HttpClient client, Image image) {
+        HttpRequest first = HttpRequest.newBuilder().uri(URI.create(image.getUrl())).build();
 
-        return client.sendAsync(first, HttpResponse.BodyHandlers.ofString())
+        long size = client.sendAsync(first, HttpResponse.BodyHandlers.ofString())
             .thenApply(HttpResponse::headers)
-            .thenApply(headers -> headers.firstValueAsLong("content-length"))
+            .thenApply(headers ->  headers.firstValueAsLong("content-length"))
             .thenApply(OptionalLong::getAsLong)
             .join();
-    }
 
-    public static String getMaxImage(HttpClient client, ArrayList<String> imageSources) {
-        long max = 0L;
-        String src = "";
-
-        for(String imageSource: imageSources) {
-            long imageSize = getImageSize(client, imageSource);
-            System.out.println("image size: " + imageSize);
-            if (max < imageSize) {
-                max = imageSize;
-                src = imageSource;
-            }
-        }
-        System.out.println("max size: " + max);
-        return src;
+        image.setSize(size);
+        System.out.println(image);
     }
 
     public String getUrlOfTheLargestPicture(int sol) {
+        // Start time
+        long startTime = System.currentTimeMillis();
+
         try (HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build()) {
 
             String url = String.format("%s?sol=%s&api_key=%s", BASE_URL, sol, API_KEY);
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
 
-            ArrayList<String> imageSources =
+            ArrayList<Image> images =
                 client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                     .thenApply(HttpResponse::body)
-                    .thenApply(NasaService::toImageSources)
+                    .thenApply(NasaService::toImages)
                     .join();
 
-            return getMaxImage(client, imageSources);
+            images.forEach(image -> fetchSize(client, image));
+
+            return images.stream()
+                .max(Comparator.comparing(Image::getSize))
+                .map(img -> {
+                    System.out.println("max: " + img);
+                    // End time
+                    long endTime = System.currentTimeMillis();
+
+                    // Calculate elapsed time in milliseconds
+                    long elapsedTime = endTime - startTime;
+
+                    System.out.println("Elapsed time: " + elapsedTime + " ms");
+                    System.out.println("Number of images: " + images.size() + " -> " + elapsedTime / images.size());
+                    return img.getUrl();
+                }).orElse(null);
         }
     }
 }
